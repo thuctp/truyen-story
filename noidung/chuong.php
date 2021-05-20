b215da8d82ed4cb2268cf27998997fa710a52c5c
minhthuc-96 <56441960+minhthuc-96@users.noreply.github.com>
2021-05-21 02:18:11 +0700

    chinh sua phan chuong

--- noidung/chuong.php
+++ noidung/chuong.php
@@ -6,6 +6,27 @@ $dsc=mysqli_fetch_array($kqchuong);
 $couterviewchuong = "UPDATE  nncms_chuong  SET SoLanXem = SoLanXem+1 WHERE idChuong = $idGetChuong";
 $kqsqlchuong=mysqli_query($con, $couterviewchuong);
 
+
+if(isset($idGetChuong))
+
+    $sodong=1; // 12 san pham ad
+$sonhom=3; // 5 trang ad
+$sl="select * from nncms_chuong where idTruyen={$dsc['idTruyen']}";
+$kqsp=mysqli_query($con, $sl);
+$tongsotrang=ceil(mysqli_num_rows($kqsp)/$sodong);
+if (!isset($_GET["p"])) //kiểm tra get so trang xuống nếu không có trang thì cho mặt định nó là
+{
+    $p=1; //cho mặt định nó là một
+}
+else
+{
+    $p=intval($_GET["p"]);
+}
+$x=($p-1)*$sodong; //lấy số trang hiện hành, để chia ra từng trang sản phẩn trong từng trang. đay là sản phẩm bắt đàu của mỗi trang
+
+$sl="select * from nncms_chuong where idTruyen={$dsc['idTruyen']} limit $x,$sodong";
+$kqsp=mysqli_query($con, $sl);
+
 ;?>
 
 
@@ -18,89 +39,103 @@ $kqsqlchuong=mysqli_query($con, $couterviewchuong);
         ?>
     </h4>
 
-    <div class="row row-cols-1 row-cols-md-4 g-2">
+
+    <div class="row">
         <?php
-        if(isset($idGetChuong))
 
-            $sodong=1; // 12 san pham ad
-        $sonhom=3; // 5 trang ad
-        $sl="select * from nncms_chuong where idTruyen={$dsc['idTruyen']}";
-        $kqsp=mysqli_query($con, $sl);
-        $tongsotrang=ceil(mysqli_num_rows($kqsp)/$sodong);
-        if (!isset($_GET["p"])) //kiểm tra get so trang xuống nếu không có trang thì cho mặt định nó là
-        {
-            $p=1; //cho mặt định nó là một
-        }
-        else
-        {
-            $p=intval($_GET["p"]);
-        }
-        $x=($p-1)*$sodong; //lấy số trang hiện hành, để chia ra từng trang sản phẩn trong từng trang. đay là sản phẩm bắt đàu của mỗi trang
-
-        $sl="select * from nncms_chuong where idTruyen={$dsc['idTruyen']} limit $x,$sodong";
-        $kqsp=mysqli_query($con, $sl);
         while ($d=mysqli_fetch_array($kqsp)) {
             ?>
+            <div class="col-12">
+                <h4 class="card-title text-primary text-center fw-600">
+                    <?php echo $d['TenChuong'];?>
+                </h4>
+                <p class="text-center fs-14"><i class="fas fa-eye fa-sm"></i> <?php echo $d['SoLanXem'];?></p>
+                <p class="text-center fs-14"><?php echo $d['NgayDang'];?></p>
 
-            <div class="col">
-                    <div class="card card-list-truyen">
-
-                        <div class="card-body">
-                            <h6 class="card-title"><?php echo $d['TenChuong'];?></h6>
-                        </div>
+                <!--pagination dropdow top-->
+                <div class="text-center mb-5 mt-4">
+                    <?php if($_GET["p"]>1){ ?>
+                        <a class="btn btn-primary"
+                           href="index.php?key=chuong&idchuong=<?php echo $idGetChuong;?>&p=<?php echo $_GET["p"]-1; ?>"><i class="fas fa-chevron-left"></i></a>
+                    <?php } ?>
+                    <div class="dropdown box-select-pagination-top">
+                        <button class="btn btn-outline-primary dropdown-toggle custom-btn" id="listchuong" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span>Chương <?php echo $_GET["p"] ?></span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="listchuong">
+                            <?php for($sochuong=1;$sochuong<=$tongsotrang;$sochuong++){ ?>
+                                <li class="<?php if($sochuong == $_GET["p"]) echo "active-select";?>">
+                                    <a href="index.php?key=chuong&idchuong=<?php echo $idGetChuong;?>&p=<?php echo $sochuong; ?>"
+                                       class="dropdown-item">Chương: <?php echo $sochuong; ?></a>
+                                </li>
+                            <?php } ?>
+                        </ul>
                     </div>
+                    <?php if($_GET["p"]<$tongsotrang){ ?>
+                        <a class="btn btn-primary" href="index.php?key=chuong&idchuong=<?php echo $idGetChuong;?>&p=<?php echo $_GET["p"]+1; ?>"><i class="fas fa-chevron-right"></i></a>
+                    <?php } ?>
+                </div>
+                <!--end pagination dropdow top-->
+
+                <div>
+                    <?php echo $d['NoiDung'];?>
+                </div>
             </div>
         <?php } ?>
 
     </div>
 
-    <div class="custom-pagination">
-        <!-- Phần hiển thị số trang -->
-        <p class="pagenavi" align="left" style="clear:both; font-weight: bold;">
-            <?php
-            $dau = $p-2;
-            if($dau<1)
-            {$dau=1;}
-            $cuoi = $dau + $sonhom - 1;
-            if($cuoi>$tongsotrang)
-            {$cuoi=$tongsotrang;}
-            ?>
-            <!-- trang hien tai cua tổng số trang -->
-            <span style="float: right;">Page <?php echo $p; ?> of <?php echo $tongsotrang;?></span>
-            <!-- hien <trang đau> hiện về trang đàu tiên -->
-
-            <!-- icon hiển thị các trang phía trước của trang hien tai -->
-            <?php if ($p>1) {
-                $p1=$p-1;
+    <!--phan phan trang -->
+    <?php if($tongsotrang>1){ ?>
+        <div class="custom-pagination">
+            <!-- Phần hiển thị số trang -->
+            <p class="pagenavi" align="left" style="clear:both; font-weight: bold;">
+                <?php
+                $dau = $p-2;
+                if($dau<1)
+                {$dau=1;}
+                $cuoi = $dau + $sonhom - 1;
+                if($cuoi>$tongsotrang)
+                {$cuoi=$tongsotrang;}
                 ?>
-                <a href="index.php?key=chuong&idchuong=<?php echo $idGetChuong;?>&p=<?php echo $p1; ?>"><i class="fas fa-chevron-left"></i></a>&nbsp;
-            <?php } ?>
-            <!-- liet ke thu tu cac trang trong nhom -->
-            <?php
-            for($i=$dau;$i<=$cuoi;$i++)
-            {
-                if ($i<=$tongsotrang)
+                <!-- trang hien tai cua tổng số trang -->
+                <span style="float: right;">Chương <?php echo $p; ?> / <?php echo $tongsotrang;?></span>
+                <!-- hien <trang đau> hiện về trang đàu tiên -->
+
+                <!-- icon hiển thị các trang phía trước của trang hien tai -->
+                <?php if ($p>1) {
+                    $p1=$p-1;
+                    ?>
+                    <a href="index.php?key=chuong&idchuong=<?php echo $idGetChuong;?>&p=<?php echo $p1; ?>"><i class="fas fa-chevron-left"></i></a>&nbsp;
+                <?php } ?>
+                <!-- liet ke thu tu cac trang trong nhom -->
+                <?php
+                for($i=$dau;$i<=$cuoi;$i++)
                 {
-                    if ($i==$p)
+                    if ($i<=$tongsotrang)
                     {
-                        echo $i."&nbsp;";
-                    }
-                    else
-                    {
-                        ?>
-                        <a href="index.php?key=chuong&idchuong=<?php echo $idGetChuong;?>&p=<?php echo $i; ?>"><?php echo $i; ?></a>
-                        <?php
+                        if ($i==$p)
+                        {
+                            echo $i."&nbsp;";
+                        }
+                        else
+                        {
+                            ?>
+                            <a href="index.php?key=chuong&idchuong=<?php echo $idGetChuong;?>&p=<?php echo $i; ?>"><?php echo $i; ?></a>
+                            <?php
+                        }
                     }
                 }
-            }
-            ?>
-            <!-- hien trang sau -->
-            <?php if ($p<$tongsotrang) {
-                $p2=$p+1;
                 ?>
-                <a href="index.php?key=chuong&idchuong=<?php echo $idGetChuong;?>&p=<?php echo $p2; ?>"><i class="fas fa-chevron-right"></i></a>&nbsp;
-            <?php } ?>
+                <!-- hien trang sau -->
+                <?php if ($p<$tongsotrang) {
+                    $p2=$p+1;
+                    ?>
+                    <a href="index.php?key=chuong&idchuong=<?php echo $idGetChuong;?>&p=<?php echo $p2; ?>"><i class="fas fa-chevron-right"></i></a>&nbsp;
+                <?php } ?>
 
-        </p>
-    </div>
+            </p>
+        </div>
+    <?php } ?>
+    <!--ket thuc phan phan trang -->
 </div>
--- noidung/danhsachloai.php
+++ noidung/danhsachloai.php
@@ -7,97 +7,110 @@
         ?>
     </h4>
 
-	<div class="row row-cols-1 row-cols-md-4 g-2">
-		<?php
-			if(isset($_GET['idcl']))
 
-			$sodong=12; // 12 san pham ad
-		 	$sonhom=3; // 5 trang ad
-			$sl="select * from nncms_truyen where idCL={$_GET['idcl']}";
-			$kqsp=mysqli_query($con, $sl);
-	 	$tongsotrang=ceil(mysqli_num_rows($kqsp)/$sodong);
-			if (!isset($_GET["p"])) //kiểm tra get so trang xuống nếu không có trang thì cho mặt định nó là
-		  {
-              $p=1; //cho mặt định nó là một
-		  }
-		  else
-		  {
-			$p=intval($_GET["p"]);
-		  }
-			$x=($p-1)*$sodong; //lấy số trang hiện hành, để chia ra từng trang sản phẩn trong từng trang. đay là sản phẩm bắt đàu của mỗi trang
+<?php
+    if(isset($_GET['idcl']))
 
-			$sl="select * from nncms_truyen where idCL={$_GET['idcl']} limit $x,$sodong";
-			$kqsp=mysqli_query($con, $sl);
-			while ($d=mysqli_fetch_array($kqsp)) {
-		?>
+    $sodong=12; // 12 san pham ad
+    $sonhom=3; // 5 trang ad
+    $sl="select * from nncms_truyen where idCL={$_GET['idcl']}";
+    $kqsp=mysqli_query($con, $sl);
+$tongsotrang=ceil(mysqli_num_rows($kqsp)/$sodong);
+    if (!isset($_GET["p"])) //kiểm tra get so trang xuống nếu không có trang thì cho mặt định nó là
+  {
+      $p=1; //cho mặt định nó là một
+  }
+  else
+  {
+    $p=intval($_GET["p"]);
+  }
+    $x=($p-1)*$sodong; //lấy số trang hiện hành, để chia ra từng trang sản phẩn trong từng trang. đay là sản phẩm bắt đàu của mỗi trang
 
-            <div class="col">
-                <a href="index.php?key=chitiettruyen&idtruyen=<?php echo $d['idTruyen'] ;?>">
-                    <div class="card card-list-truyen">
-                        <div class="wrap-card-img"><img class="card-img-top"
-                                                        src="upload/sanpham/<?php echo $d['UrlHinh'];?>" alt="hinh"></div>
-                        <div class="card-body">
-                            <h6 class="card-title"><?php echo $d['TenTruyen'];?></h6>
-                        </div>
-                    </div>
-                </a>
+    $sl="select * from nncms_truyen where idCL={$_GET['idcl']} limit $x,$sodong";
+    $kqsp=mysqli_query($con, $sl);
+    $checkDATA=mysqli_num_rows($kqsp);
+    if($checkDATA > 0){ ?>
+    <div class="row row-cols-1 row-cols-md-4 g-2">
+    <?php while ($d=mysqli_fetch_array($kqsp)) { ?>
+
+    <div class="col">
+        <a href="index.php?key=chitiettruyen&idtruyen=<?php echo $d['idTruyen'] ;?>">
+            <div class="card card-list-truyen">
+                <div class="wrap-card-img"><img class="card-img-top"
+                                                src="upload/sanpham/<?php echo $d['UrlHinh'];?>" alt="hinh"></div>
+                <div class="card-body">
+                    <h6 class="card-title"><?php echo $d['TenTruyen'];?></h6>
+                </div>
             </div>
-		<?php } ?>
+        </a>
+    </div>
+    <?php } ?>
+    </div>
 
-	</div>
+    <!--phan phan trang-->
+    <?php if($tongsotrang>1){ ?>
+    <div class="custom-pagination">
+        <!-- Phần hiển thị số trang -->
+        <p class="pagenavi" align="left" style="clear:both; font-weight: bold;">
+            <?php
+            $dau = $p-2;
+            if($dau<1)
+            {$dau=1;}
+            $cuoi = $dau + $sonhom - 1;
+            if($cuoi>$tongsotrang)
+            {$cuoi=$tongsotrang;}
+            ?>
+            <!-- trang hien tai cua tổng số trang -->
+            <span style="float: right;">Page <?php echo $p; ?> of <?php echo $tongsotrang;?></span>
+            <!-- hien <trang đau> hiện về trang đàu tiên -->
+            <a href="index.php?key=truyenCL&idcl=<?php echo $_GET['idcl'];?>&p=1">Trang đầu</a>
+            <!-- icon hiển thị các trang phía trước của trang hien tai -->
+            <?php if ($p>1) {
+                $p1=$p-1;
+                ?>
+                <a href="index.php?key=truyenCL&idcl=<?php echo $_GET['idcl'];?>&p=<?php echo $p1; ?>"><img src="images/icon/1328101938_Arrow-Right.png" width="40" height="20"/></a>&nbsp;
+            <?php } ?>
+            <!-- liet ke thu tu cac trang trong nhom -->
+            <?php
+            for($i=$dau;$i<=$cuoi;$i++)
+            {
+                if ($i<=$tongsotrang)
+                {
+                    if ($i==$p)
+                    {
+                        echo $i."&nbsp;";
+                    }
+                    else
+                    {
+                        ?>
+                        <a href="index.php?key=truyenCL&idcl=<?php echo $_GET['idcl'];?>&p=<?php echo $i; ?>"><?php echo $i; ?></a>
+                        <?php
+                    }
+                }
+            }
+            ?>
+            <!-- hien trang sau -->
+            <?php if ($p<$tongsotrang) {
+                $p2=$p+1;
+                ?>
+                <a href="index.php?key=truyenCL&idcl=<?php echo $_GET['idcl'];?>&p=<?php echo $p2; ?>"><img src="images/icon/1328101938_Arrow-left.png" width="40" height="20" /></a>&nbsp;
+            <?php } ?>
+            <?php
+            if($p=$tongsotrang){
+                $trangcuoi=$p;
+                ?>
+                <a href="index.php?key=truyenCL&idcl=<?php echo $_GET['idcl'];?>&p=<?php echo $trangcuoi; ?>">Trang cuối</a>
+            <?php } ?>
+        </p>
+    </div>
+    <?php } ?>
+    <!--ket thuc phan phan trang-->
 
-	<div class="custom-pagination">
-	<!-- Phần hiển thị số trang -->
-	<p class="pagenavi" align="left" style="clear:both; font-weight: bold;">
-	<?php
-		$dau = $p-2;
-		if($dau<1)
-			{$dau=1;}
-			$cuoi = $dau + $sonhom - 1;
-		if($cuoi>$tongsotrang)
-			{$cuoi=$tongsotrang;}
-	?>
-		<!-- trang hien tai cua tổng số trang -->
-	    <span style="float: right;">Page <?php echo $p; ?> of <?php echo $tongsotrang;?></span>
-		<!-- hien <trang đau> hiện về trang đàu tiên -->
-	    <a href="index.php?key=truyenCL&idcl=<?php echo $_GET['idcl'];?>&p=1">Trang đầu</a>
-		<!-- icon hiển thị các trang phía trước của trang hien tai -->
-	<?php if ($p>1) {
-		$p1=$p-1;
-	?>
-		<a href="index.php?key=truyenCL&idcl=<?php echo $_GET['idcl'];?>&p=<?php echo $p1; ?>"><img src="images/icon/1328101938_Arrow-Right.png" width="40" height="20"/></a>&nbsp;
-	<?php } ?>
-		<!-- liet ke thu tu cac trang trong nhom -->
-	<?php
-		for($i=$dau;$i<=$cuoi;$i++)
-		{
-		if ($i<=$tongsotrang)
-		{
-		if ($i==$p)
-		   {
-		   		echo $i."&nbsp;";
-		   }
-		   else
-		   {
-	 ?>
-	 	<a href="index.php?key=truyenCL&idcl=<?php echo $_GET['idcl'];?>&p=<?php echo $i; ?>"><?php echo $i; ?></a>
-	<?php
-	   		}
-		  }
-		}
-	?>
-	<!-- hien trang sau -->
-	<?php if ($p<$tongsotrang) {
-		$p2=$p+1;
-	?>
-		<a href="index.php?key=truyenCL&idcl=<?php echo $_GET['idcl'];?>&p=<?php echo $p2; ?>"><img src="images/icon/1328101938_Arrow-left.png" width="40" height="20" /></a>&nbsp;
-	<?php } ?>
-	<?php
-	    if($p=$tongsotrang){
-	        $trangcuoi=$p;
-	?>
-	    <a href="index.php?key=truyenCL&idcl=<?php echo $_GET['idcl'];?>&p=<?php echo $trangcuoi; ?>">Trang cuối</a>
-	<?php } ?>
-	</p>
-	</div>
+    <?php  } else{ ?>
+        <div class="bg-white p-4">
+            <h6>Không Có truyện nào cùng Thể Loại này!</h6>
+        </div>
+    <?php } ?>
+
+
 </div>
