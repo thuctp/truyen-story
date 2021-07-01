0de761439997fc781555ec2921f07d7e56474e0c
minhthuc-96 <56441960+minhthuc-96@users.noreply.github.com>
2021-07-02 00:02:14 +0700

    chinh sua comment

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
-//@mysqli_connect("localhost","root","hhh") or die("Connection failed");
-//mysqli_select_db("webbanhang");
-//mysqli_query("set names 'utf8'");
-
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $con = mysqli_connect('localhost', 'root', '');
 mysqli_select_db($con, 'test')
+
+//mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+//$con = mysqli_connect('localhost', 'tru86047_admin01', 'Minhthuc#1996');
+//mysqli_select_db($con, 'tru86047_truyen01')
 ?>
